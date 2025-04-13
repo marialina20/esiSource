2538c1fdc0bf0b85b63370f9bd501a986712dd7d
mariardj <127994063+mariardj@users.noreply.github.com>
2025-04-14 00:56:28 +0100

    userhome page

--- frontesisource/src/App.js
+++ frontesisource/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginP from './pages/LoginP';
 import Navvbar from './pages/Navvbar';
+import UserHomeP from './pages/UserHomeP';
 import { Link } from 'react-router-dom';
 function App() {
   return (
    <div>  
    <BrowserRouter >
         <Routes >
-          {<Route index element ={<Navvbar/>}/> } 
+          {<Route index element ={<UserHomeP/>}/> } 
         </Routes>
     </BrowserRouter>
 
