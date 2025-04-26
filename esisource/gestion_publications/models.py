7e552a31d9af5dbfc564bc758fc4b2b8d2a51965
CHERABRAB-lilia <126592729+CHERABRAB-lilia@users.noreply.github.com>
2025-04-26 17:56:10 +0100

    Sauvegarde avant pull

--- esisource/gestion_publications/models.py
+++ esisource/gestion_publications/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import mimetypes
 
+
 class Publication(models.Model):
     STATUT_CHOICES = [
         ('brouillon', 'Brouillon'),
